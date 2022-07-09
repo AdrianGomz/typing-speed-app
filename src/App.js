308f11d752f34ff29de93b615cb8f65fe1fe5823
isaac <vi13150@innovaccion.mx>
2022-07-08 22:32:23 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+const quote =
+  "She believed, and was entitled to believe, I must say, that all human beings were evil by nature, whether tormentors or victims, or idle standers-by. They could only create meaningless tragedies, she said, since they weren't nearly intelligent enough to accomplish all the good they were meant to do.";
 
 function App() {
+  const [text, setText] = useState("");
+  const handleTyping = (e) => {
+    setText(e.target.value);
+    setCurrentWord(e.target.value.split(" ").length - 1);
+  };
+  const [currentWord, setCurrentWord] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Quote">
+        {quote.split(" ").map((word, i) => {
+          return (
+            <span key={i} className={currentWord === i ? "current" : null}>
+              {word}{" "}
+            </span>
+          );
+        })}
+      </div>
+      <input
+        type="text"
+        value={text}
+        onChange={handleTyping}
+        className="TypingInput"
+      />
     </div>
   );
 }
